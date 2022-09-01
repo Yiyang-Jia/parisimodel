69ddfd25157249f1b5e91ba1c3e476f37bdd0949
Yiyang-Jia <yyjiahbar@gmail.com>
2022-09-01 16:30:41 +0300

    Add files via upload

---
+++ MATLAB/seeded/0p3pi/hamiSymGauge14to260p3Pi2relsseed129.m
@@ -0,0 +1,57 @@
+dmax=100;
+%q = sin(phi)/(phi);
+%flux = ranPhases(dmax,phi);
+rais = (pauli(1) + 1i*pauli(2))/2;
+%fileID = fopen('parisiLowestEigs0p05PidStartsAt14.txt','w');
+%fileID2 = fopen('parisiOnlyLowestEigs0p1PidStartsAt24.txt','w');
+%fileID3 = fopen('parisiLowestEigs0p3PidStartsAt14.txt','w');
+
+phi = 0.3 * pi;
+fileID0p3Pi3r = fopen('parisiDiscreteFluxLowestEigs0p3Pi2Realizations14to26seed129.txt','w');
+%fidrel = fopen('relcount.txt','w');
+rng(129);
+for rel =1:2
+    flux = ranPhasesDiscrete(dmax,phi);
+for d =14:26
+%emax = sqrt(4*d/(1-q));
+%qRen = q -(q+1)/d;
+%emaxRen=sqrt(4*d/(1-qRen));
+hamiP = sparse(zeros(2,2));
+for k=2:d
+    hamiP = sparse(kron(hamiP,zeros(2,2)));
+end
+sigList = cell(1,d);
+  for m=1:d
+      for n =1:d
+          sigList{n}  = [exp(1i* flux(m,n)/4),0;0,exp(-1i* flux(m,n)/4)];
+      end
+      sigList{m} = rais;
+      
+      gamP = sigList{1};
+      for s = 2:d
+          gamP = sparse(kron(gamP,sigList{s}));
+      end
+      hamiP = hamiP + gamP;
+  end
+  hami  = hamiP + hamiP';
+
+ev=eigs(hami,4);
+ev=ev(ev>0)
+
+%tic
+%[vec,lambda]=lobpcg(rand(2^d,2),hami);
+%lambda
+
+%fprintf(fileID,'%.10f\n', ev);
+%fprintf(fileID2,'%.10f\n', ev);
+%fprintf(fileID3,'%.10f\n', ev);
+fprintf(fileID0p3Pi3r,'%.10f\n', ev);
+%d
+end
+%fprintf(fidrel,rel);
+end
+%fclose(fileID);
+%fclose(fileID2);
+%fclose(fileID3);
+fclose(fileID0p3Pi3r);
+%fclose(fidrel);
